a4ac39b870b4de623b6e7e76df1fc10d919910ce
hjunleon <hjunleon@gmail.com>
2022-05-24 12:21:00 +0800

    Landing page more or less done

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -480,10 +480,19 @@ function App(props) {
 
   return (
     <div className="App">
-      {/* ✏️ Edit the header and change the title to your project name */}
-      <Header />
       {networkDisplay}
       <BrowserRouter>
+        <Header
+          address={address}
+          localProvider={localProvider}
+          userSigner={userSigner}
+          mainnetProvider={mainnetProvider}
+          price={price}
+          web3Modal={web3Modal}
+          loadWeb3Modal={loadWeb3Modal}
+          logoutOfWeb3Modal={logoutOfWeb3Modal}
+          blockExplorer={blockExplorer}
+        />
         {/* <Menu style={{ textAlign: "center" }} selectedKeys={[route]} mode="horizontal">
           <Menu.Item key="/">
             <Link
@@ -511,8 +520,6 @@ function App(props) {
           <Route exact path="/">
             {completeDisplay}
             <Home yourLocalBalance={yourLocalBalance} readContracts={readContracts} />
-
-            
 
             {/* uncomment for a second contract:
             <Contract
@@ -547,62 +554,6 @@ function App(props) {
       </BrowserRouter>
 
       <ThemeSwitch />
-
-      {/* 👨‍💼 Your account is in the top right with a wallet at connect options */}
-      <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
-        <Account
-          address={address}
-          localProvider={localProvider}
-          userSigner={userSigner}
-          mainnetProvider={mainnetProvider}
-          price={price}
-          web3Modal={web3Modal}
-          loadWeb3Modal={loadWeb3Modal}
-          logoutOfWeb3Modal={logoutOfWeb3Modal}
-          blockExplorer={blockExplorer}
-        />
-        {faucetHint}
-      </div>
-
-      {/* 🗺 Extra UI like gas price, eth price, faucet, and support: */}
-      <div style={{ position: "fixed", textAlign: "left", left: 0, bottom: 20, padding: 10 }}>
-        <Row align="middle" gutter={[4, 4]}>
-          <Col span={8}>
-            <Ramp price={price} address={address} networks={NETWORKS} />
-          </Col>
-
-          <Col span={8} style={{ textAlign: "center", opacity: 0.8 }}>
-            <GasGauge gasPrice={gasPrice} />
-          </Col>
-          <Col span={8} style={{ textAlign: "center", opacity: 1 }}>
-            <Button
-              onClick={() => {
-                window.open("https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA");
-              }}
-              size="large"
-              shape="round"
-            >
-              <span style={{ marginRight: 8 }} role="img" aria-label="support">
-                💬
-              </span>
-              Support
-            </Button>
-          </Col>
-        </Row>
-
-        <Row align="middle" gutter={[4, 4]}>
-          <Col span={24}>
-            {
-              /*  if the local provider has a signer, let's show the faucet:  */
-              faucetAvailable ? (
-                <Faucet localProvider={localProvider} price={price} ensProvider={mainnetProvider} />
-              ) : (
-                ""
-              )
-            }
-          </Col>
-        </Row>
-      </div>
     </div>
   );
 }
--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,16 +1,79 @@
-import { PageHeader } from "antd";
+import { Menu, PageHeader } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
 import React from "react";
-
+import Account from "./Account";
 // displays a page header
 
-export default function Header() {
+export default function Header({
+  address,
+  localProvider,
+  userSigner,
+  mainnetProvider,
+  price,
+  web3Modal,
+  loadWeb3Modal,
+  logoutOfWeb3Modal,
+  blockExplorer,
+}) {
   return (
-    <a href="/" /*target="_blank" rel="noopener noreferrer"*/>
-      <PageHeader
-        title="🏗 Peanuts Vault"
-        subTitle="A simple, secure, and easy to use vault for your Peanuts"
-        style={{ cursor: "pointer" }}
-      />
-    </a>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <a
+        href="/"
+        style={{
+          display: "flex",
+        }}
+      >
+        <img
+          src="/proudPeanutNoBackground.png"
+          style={{
+            height: "80px",
+            marginRight: "12px",
+          }}
+        />
+        <PageHeader title="Peanuts.Finance" style={{ cursor: "pointer", marginLeft: "0" }} />
+      </a>
+      <div
+        style={{
+          display: "flex",
+          padding: "0 16px",
+          alignItems: "center",
+        }}
+      >
+        <Menu
+          style={{
+            display: "flex",
+            alignItems: "center",
+            background: "none",
+          }}
+        >
+          <Menu.Item>
+            <GithubOutlined
+              style={{
+                fontSize: "2em",
+                verticalAlign: "center"
+              }}
+            />
+          </Menu.Item>
+          <Menu.Item>Learn more</Menu.Item>
+        </Menu>
+        <Account
+          address={address}
+          localProvider={localProvider}
+          userSigner={userSigner}
+          mainnetProvider={mainnetProvider}
+          price={price}
+          web3Modal={web3Modal}
+          loadWeb3Modal={loadWeb3Modal}
+          logoutOfWeb3Modal={logoutOfWeb3Modal}
+          blockExplorer={blockExplorer}
+        />
+      </div>
+    </div>
   );
 }
